ebc56f3667e67f3c226896400eac85cf676db8b8
“Corinne <corinne.canavan@gmail.com>
2023-01-12 18:39:58 -0700

    Add useEffect to App to set data to state

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,6 @@
 import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const ALL_USERS_QUERY = gql`
@@ -15,8 +15,25 @@ const ALL_USERS_QUERY = gql`
 
 const App = () => {
   const { loading, error, data } = useQuery(ALL_USERS_QUERY);
+  const [allUsersData, setAllUsersData] = useState([])
 
-  if (loading) {
+  // if (loading) {
+  //   return <p>Loading...</p>;
+  // }
+
+  // if (error) {
+  //   return <p>Error: {JSON.stringify(error)}</p>;
+  // }
+
+  useEffect(() => {
+    console.log("data before if", data)
+    if (data) {
+      console.log("data after if", data)
+      setAllUsersData(data.allUsers)
+    }
+  }, [data])
+
+    if (loading) {
     return <p>Loading...</p>;
   }
 
@@ -27,7 +44,7 @@ const App = () => {
   return (
     <pre>
       <code>
-        {JSON.stringify(data, null, 2)}
+        {JSON.stringify(allUsersData, null, 2)}
       </code>
     </pre>
   )
