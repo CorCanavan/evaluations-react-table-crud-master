72b6ef33b28823e3004ceedd42516e9b26387fdf
“Corinne <corinne.canavan@gmail.com>
2023-01-20 12:59:09 -0700

    Refactor Row component

--- src/Components/Container/Container.js
+++ src/Components/Container/Container.js
@@ -4,26 +4,26 @@ import Row from '../Row/Row.js';
 
 const Container = ({ allUsersData }) => {
 
-  // const allUsersRows = allUsersData.map(user => {
-  //   return (
-  //     <Row
-  //       key={user.email}
-  //       email={user.email}
-  //       name={user.name}
-  //       role={user.role}
-  //     />
-  //   )
-  // })
-
   const allUsersRows = allUsersData.map(user => {
     return (
-      <tr key={user.email}>
-        <td>{user.email}</td>
-        <td>{user.name}</td>
-        <td>{user.role}</td>
-      </tr>
+      <Row
+        key={user.email}
+        email={user.email}
+        name={user.name}
+        role={user.role}
+      />
     )
   })
+
+  // const allUsersRows = allUsersData.map(user => {
+  //   return (
+  //     <tr key={user.email}>
+  //       <td>{user.email}</td>
+  //       <td>{user.name}</td>
+  //       <td>{user.role}</td>
+  //     </tr>
+  //   )
+  // })
 
   return (
     // <div className="container">
--- src/Components/Row/Row.js
+++ src/Components/Row/Row.js
@@ -4,17 +4,30 @@ import './Row.css';
 const Row = ({ email, name, role }) => {
 
   return (
-    <article className="row">
-      <label className="info">
-        <input 
-          type="checkbox" 
-        />
-        {email}
-      </label>
-      {/* <p className="info">{email}</p> */}
-      <p className="info">{name}</p>
-      <p className="info">{role}</p>
-    </article>
+    // <article className="row">
+    //   <label className="info">
+    //     <input 
+    //       type="checkbox" 
+    //     />
+    //     {email}
+    //   </label>
+    //   {/* <p className="info">{email}</p> */}
+    //   <p className="info">{name}</p>
+    //   <p className="info">{role}</p>
+    // </article>
+
+    <tr key={email}>
+      <td>
+        <label>
+          <input 
+            type="checkbox" 
+          />
+          {email}
+        </label>
+      </td>
+      <td>{name}</td>
+      <td>{role}</td>
+    </tr>
 
   )
 }
