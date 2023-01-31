f4df3bbe19f4c1260da7fd3f4dd4c714d50ba87f
“Corinne <corinne.canavan@gmail.com>
2023-01-31 10:54:15 -0700

    Add initial styling to role section

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -3,30 +3,18 @@ import './Details.css';
 
 const Details = ({ userToEdit, allRoles, formatRole}) => {
 
-  // const formatRole = (enumRole) => {
-  //   const revisedRole = enumRole.split("_")
-  //   return revisedRole.map(role => {
-  //     return role[0] + role.substring(1).toLowerCase();
-  //   }).join(" ")
-  // }
-
-  // console.log("formatRole", formatRole('APP_MANAGER'))
-
-  console.log("allRoles", allRoles)
-
   const roleRadioButtons = allRoles.map(enumRole => {
-    console.log("enumRole", enumRole)
-    console.log("fn", formatRole(enumRole.name))
     return (
       <label className="radio-label">
         <input
+          className="radio-input"
           key={enumRole.name}
           type="radio"
           name="role"
           value={enumRole.name}
           checked={userToEdit.role === formatRole(enumRole.name)}
-    />
-      {formatRole(enumRole.name)}
+        />
+        {formatRole(enumRole.name)}
       </label>
     )
   })
@@ -46,7 +34,10 @@ const Details = ({ userToEdit, allRoles, formatRole}) => {
           />
       </article>
       <article className="role-article">
-        {roleRadioButtons}
+        <label className="role-input-label">
+          Role
+        </label>
+          {roleRadioButtons}
       </article>
     </section>
   )
