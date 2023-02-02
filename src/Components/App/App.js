a75ccd210ac4d15b95889c2407b9534d654f14aa
“Corinne <corinne.canavan@gmail.com>
2023-02-02 11:21:21 -0700

    Enable Prettier; connect to prettier config file

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,7 +7,6 @@ import Container from '../Container/Container';
 import Details from '../Details/Details';
 import { Route, useHistory } from 'react-router-dom';
 
-
 const ALL_USERS_QUERY = gql`
   query {
     allUsers {
@@ -43,10 +42,8 @@ const ROLES_QUERY = gql`
 
 const UPDATE_USER = gql`
   mutation UpdateUser($email: ID!, $newAttributes: UserAttributesInput!) {
-    updateUser(email: $email,
-        newAttributes: $newAttributes
-    ) {
-      name,
+    updateUser(email: $email, newAttributes: $newAttributes) {
+      name
       role
     }
   }
@@ -61,19 +58,21 @@ const App = () => {
   const [userToEdit, setUserToEdit] = useState({});
   const [deleteUsers] = useMutation(DELETE_USERS);
   const [resetUsers] = useMutation(RESET_USERS);
-  const [updateUser] = useMutation(UPDATE_USER)
+  const [updateUser] = useMutation(UPDATE_USER);
   const history = useHistory();
 
   const formatRole = (enumRole) => {
-    const revisedRole = enumRole.split("_")
-    return revisedRole.map(role => {
-      return role[0] + role.substring(1).toLowerCase();
-    }).join(" ")
-  }
-  
+    const revisedRole = enumRole.split('_');
+    return revisedRole
+      .map((role) => {
+        return role[0] + role.substring(1).toLowerCase();
+      })
+      .join(' ');
+  };
+
   useEffect(() => {
-    resetUsers(true)
-    console.log('meow')
+    resetUsers(true);
+    console.log('meow');
     if (usersData && rolesData) {
       const formattedData = usersData.allUsers.map((user, index) => {
         return {
@@ -83,15 +82,15 @@ const App = () => {
           // role: formatRole(user.role),
           role: user.role,
           typename: user.__typename,
-          isChecked: false
-        }
-      })
-      setAllUsersData(formattedData)
-      setAllRoles(rolesData.__type.enumValues)
+          isChecked: false,
+        };
+      });
+      setAllUsersData(formattedData);
+      setAllRoles(rolesData.__type.enumValues);
     }
-  }, [usersData])
+  }, [usersData]);
 
-    if (usersLoading || rolesLoading) {
+  if (usersLoading || rolesLoading) {
     return <p>Loading...</p>;
   }
 
@@ -104,113 +103,113 @@ const App = () => {
   }
 
   const handleSelectedUsers = (revisedUsers) => {
-    const filterByChecked = revisedUsers.filter(user => user.isChecked)
-    setSelectedUsers(filterByChecked)
-  }
+    const filterByChecked = revisedUsers.filter((user) => user.isChecked);
+    setSelectedUsers(filterByChecked);
+  };
 
   const handleCheck = (id) => {
-    const revisedUsers = allUsersData.map(user => {
+    const revisedUsers = allUsersData.map((user) => {
       if (user.id === id) {
         const updatedUser = {
           ...user,
-          isChecked : !user.isChecked
-        }
+          isChecked: !user.isChecked,
+        };
         return updatedUser;
       } else {
         return user;
       }
-    })
+    });
     setAllUsersData(revisedUsers);
     handleSelectedUsers(revisedUsers);
-  }
+  };
 
   const handleDelete = () => {
-    const emails = selectedUsers.map(selectedUser => {
+    const emails = selectedUsers.map((selectedUser) => {
       return selectedUser.email;
-    })
-    const filteredUsers = allUsersData.filter(user => !user.isChecked)
-    deleteUsers({ variables : { emails }})
-    setAllUsersData(filteredUsers)
-    setSelectedUsers([])
-  }
+    });
+    const filteredUsers = allUsersData.filter((user) => !user.isChecked);
+    deleteUsers({ variables: { emails } });
+    setAllUsersData(filteredUsers);
+    setSelectedUsers([]);
+  };
 
   const handleEditUser = (name) => {
-    const findUserByName = allUsersData.find(user => user.name === name)
+    const findUserByName = allUsersData.find((user) => user.name === name);
     // findUserByName.role = findUserByName.enumRole
-    console.log("findUserByName", findUserByName)
-    setUserToEdit(findUserByName)
+    console.log('findUserByName', findUserByName);
+    setUserToEdit(findUserByName);
     history.push(`user/${findUserByName.email}`);
-  }
+  };
 
   const clearUserToEdit = () => {
-    setUserToEdit({})
-  }
+    setUserToEdit({});
+  };
 
   const handleUpdatedUser = () => {
     const userAttrInput = {
       name: userToEdit.name,
-      role: userToEdit.role
-    }
+      role: userToEdit.role,
+    };
     // updateUser({ variables: { email: userToEdit.email, newAttributes: userAttrInput }, refetchQueries: [{query: ALL_USERS_QUERY}] })
 
-    updateUser({ variables: { email: userToEdit.email, newAttributes: userAttrInput } })
+    updateUser({ variables: { email: userToEdit.email, newAttributes: userAttrInput } });
 
-    console.log("allUsers1", allUsersData)
-    const updatedAllUsers = allUsersData.map(user => {
+    console.log('allUsers1', allUsersData);
+    const updatedAllUsers = allUsersData.map((user) => {
       if (user.email === userToEdit.email) {
         return {
           ...user,
           name: userToEdit.name,
           role: userToEdit.role,
           // isChecked: userToEdit.isChecked
-        }
+        };
       } else {
         return user;
       }
-    })
-    setAllUsersData(updatedAllUsers)
+    });
+    setAllUsersData(updatedAllUsers);
     clearUserToEdit();
-    console.log("allUsers2", allUsersData)
-  }
+    console.log('allUsers2', allUsersData);
+  };
 
   return (
     <main>
       <Route
-        exact path="/"
+        exact
+        path="/"
         render={() => {
-          return <section className="content-container">
-            <Header 
-              selectedUsers={selectedUsers} 
-              handleDelete={handleDelete} 
-            />
-            <Container 
-              allUsersData={allUsersData} 
-              handleCheck={handleCheck} 
-              handleEditUser={handleEditUser} 
-              formatRole={formatRole}
-            />
-          </section>
+          return (
+            <section className="content-container">
+              <Header selectedUsers={selectedUsers} handleDelete={handleDelete} />
+              <Container
+                allUsersData={allUsersData}
+                handleCheck={handleCheck}
+                handleEditUser={handleEditUser}
+                formatRole={formatRole}
+              />
+            </section>
+          );
         }}
       />
       <Route
-        exact path="/user/:email"
+        exact
+        path="/user/:email"
         render={() => {
-          return <section className="content-container">
-            <Header 
-              userToEdit={userToEdit} 
-              handleUpdatedUser={handleUpdatedUser}
-            />
-            <Details
-              setUserToEdit={setUserToEdit} 
-              userToEdit={userToEdit} 
-              allRoles={allRoles} 
-              formatRole={formatRole} 
-            />
-          </section>
+          return (
+            <section className="content-container">
+              <Header userToEdit={userToEdit} handleUpdatedUser={handleUpdatedUser} />
+              <Details
+                setUserToEdit={setUserToEdit}
+                userToEdit={userToEdit}
+                allRoles={allRoles}
+                formatRole={formatRole}
+              />
+            </section>
+          );
         }}
       />
     </main>
-  )
-}
+  );
+};
 
 export default App;
